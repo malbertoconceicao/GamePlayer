7255e62ed866a677e8b5c7e66754cae2fcaa41ce
malbertoconceicao <72625057+malbertoconceicao@users.noreply.github.com>
2022-08-01 11:47:09 -0300

    UserConfiguration

--- Program.cs
+++ Program.cs
@@ -5,19 +5,11 @@ namespace GamePlayer
   {
     static void Main(string[] args)
     {
+      var player = new PlayerOne();
       Console.Clear();
-      Console.WriteLine("what is the name of the first player?");
-      var playerOneName = Console.ReadLine();
-      var playerOne = new PlayerOne();
-      playerOne.PlayerName = playerOneName;
+      Console.WriteLine("Olá {0} ", Environment.UserName);
+      Thread.Sleep(3000);
 
-      Console.WriteLine("what is the name of the second player?");
-      var playerTwoName = Console.ReadLine();
-      var playerTwo = new PlayerTwo();
-      playerTwo.PlayerName = playerTwoName;
-
-      Console.WriteLine(playerOne.PlayerName);
-      Console.WriteLine(playerTwo.PlayerName);
       Random dados = new Random();
 
       for (int i = 0; i < 4; i++)
@@ -30,7 +22,6 @@ namespace GamePlayer
       Console.Clear();
       Console.WriteLine("Dados parados");
       Console.WriteLine(dados.Next(0, 10));
-
     }
   }
 }
