99579ce38d5fdfdad66d38e78a9fe2b53656663f
malbertoconceicao <72625057+malbertoconceicao@users.noreply.github.com>
2022-08-03 10:50:49 -0300

    Menus

--- Menus.cs
+++ Menus.cs
@@ -3,6 +3,14 @@ namespace GamePlayer
 {
   class Menus
   {
-
+    public static void MenuPrincipal()
+    {
+      Console.WriteLine("Bem vindo {0}", Environment.UserName);
+      Console.WriteLine("Escolha uma das opçoes abaixo.");
+      Console.WriteLine("1 - Jogar");
+      Console.WriteLine("2 - Regras do Jogo");
+      Console.WriteLine("3 - Sair");
+      var opcao = Console.ReadLine();
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -5,20 +5,7 @@ namespace GamePlayer
   {
     static void Main(string[] args)
     {
-      var player = new PlayerOne();
-      Console.Clear();
-      Console.WriteLine("Olá {0} ", Environment.UserName);
-      Thread.Sleep(3000);
-
-      Random dados = new Random();
-
-      for (int i = 0; i < 4; i++)
-      {
-        Console.Clear();
-        Console.WriteLine("Dados rolando");
-        Console.WriteLine(dados.Next(0, 10));
-        Thread.Sleep(1000);
-      }
+      Menus.MenuPrincipal();
     }
   }
 }
